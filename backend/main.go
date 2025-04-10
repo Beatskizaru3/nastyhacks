483cde565f00fabd23daf92e9a7df4d060b10b57
ahmed <beatskizaru3@gmail.com>
2025-04-10 22:42:25 +0300

    1

--- backend/main.go
+++ backend/main.go
@@ -1,5 +1,28 @@
 package main
 
-func main() {
+import "github.com/gin-gonic/gin"
 
+func main() {
+	router := gin.Default()
+
+	router.GET("/files", func(c *gin.Context) {
+		//получаемв ответ полный список файлов
+		var request string
+		c.ShouldBindJSON(&request)
+
+		c.JSON(200, gin.H{"response": request})
+	})
+
+	router.GET("/files/:filename??", func(ctx *gin.Context) {
+		//из тела запроса считывалось какой айдишник файла
+		//и затем искало файл по айди и скачивало
+
+	})
+	router.POST("/upload", func(ctx *gin.Context) {
+
+		//считывало тело запроса, распаршивало его в структуру с биндами
+		//и затем сохраняло поле из структуры в папку отдельную
+		//файл должен иметь свой уникальный айли для того чтобы юзеры могли его скачать
+	})
+	router.Run()
 }
