07171572251b82fecd88bfe9b72545553e00cef5
beatskizaru3 <beatskizaru3@gmail.com>
2025-06-10 15:43:21 +0300

    1

---
+++ nastyhacks/src/App.js
@@ -0,0 +1,155 @@
+import './index.scss';
+import './styles/normalize.scss';
+import './styles/media.scss';
+import './styles/utility.scss';
+import './styles/main.scss';
+import './styles/header.scss';
+
+import Header from './components/Header';
+
+function App() {
+  return (
+  <>
+    <Header/>
+   <main className='main container'>
+      <div className="main__top">
+        <h1 className="main__top-title">Found Posts</h1>
+        <div className="main__top-filter">Filter<img src='/svg/filter.svg' alt=""/></div>
+      </div>
+      <section className="main__cards">
+        <div className="main__card">
+          <img src="https://scriptrb.com/cdn-cgi/image/medium-low,format=auto,height=420/https://scriptrb.com/img/uploads/684692b90d5e1_106989035172456074720.webp" alt="" className="main__card-img" />
+          <div className="main__card-body">
+            <div className="main__card-title">Factory RNG</div>
+            <p className="main__card-description">Lorem ipsum dolor sit amet, consectetur adipisicing elit. Praesentium, exercitationem explicabo nisi aut laborum veritatis ad velit nihil sunt aliquam inventore a nulla mollitia quae, minus accusamus animi eos et.</p>
+            <div className="main__card-additional">
+              <button>View</button>
+              <div className="main__card-stat">
+                <p className="main__card-stat-downloads">131</p>
+                <p className="main__card-stat-date">09.06.2025</p>
+              </div>
+            </div>
+            </div>
+          </div>
+          <div className="main__card">
+          <img src="https://scriptrb.com/cdn-cgi/image/medium-low,format=auto,height=420/https://scriptrb.com/img/uploads/684692b90d5e1_106989035172456074720.webp" alt="" className="main__card-img" />
+          <div className="main__card-body">
+            <div className="main__card-title">Factory RNG</div>
+            <p className="main__card-description">Lorem ipsum dolor sit amet, consectetur adipisicing elit. Praesentium, exercitationem explicabo nisi aut laborum veritatis ad velit nihil sunt aliquam inventore a nulla mollitia quae, minus accusamus animi eos et.</p>
+            <div className="main__card-additional">
+              <button>View</button>
+              <div className="main__card-stat">
+                <p className="main__card-stat-downloads">131</p>
+                <p className="main__card-stat-date">09.06.2025</p>
+              </div>
+            </div>
+            </div>
+          </div>
+          <div className="main__card">
+          <img src="https://scriptrb.com/cdn-cgi/image/medium-low,format=auto,height=420/https://scriptrb.com/img/uploads/684692b90d5e1_106989035172456074720.webp" alt="" className="main__card-img" />
+          <div className="main__card-body">
+            <div className="main__card-title">Factory RNG</div>
+            <p className="main__card-description">Lorem ipsum dolor sit amet, consectetur adipisicing elit. Praesentium, exercitationem explicabo nisi aut laborum veritatis ad velit nihil sunt aliquam inventore a nulla mollitia quae, minus accusamus animi eos et.</p>
+            <div className="main__card-additional">
+              <button>View</button>
+              <div className="main__card-stat">
+                <p className="main__card-stat-downloads">131</p>
+                <p className="main__card-stat-date">09.06.2025</p>
+              </div>
+            </div>
+            </div>
+          </div>
+          <div className="main__card">
+          <img src="https://scriptrb.com/cdn-cgi/image/medium-low,format=auto,height=420/https://scriptrb.com/img/uploads/684692b90d5e1_106989035172456074720.webp" alt="" className="main__card-img" />
+          <div className="main__card-body">
+            <div className="main__card-title">Factory RNG</div>
+            <p className="main__card-description">Lorem ipsum dolor sit amet, consectetur adipisicing elit. Praesentium, exercitationem explicabo nisi aut laborum veritatis ad velit nihil sunt aliquam inventore a nulla mollitia quae, minus accusamus animi eos et.</p>
+            <div className="main__card-additional">
+              <button>View</button>
+              <div className="main__card-stat">
+                <p className="main__card-stat-downloads">131</p>
+                <p className="main__card-stat-date">09.06.2025</p>
+              </div>
+            </div>
+            </div>
+          </div>
+          <div className="main__card">
+          <img src="https://scriptrb.com/cdn-cgi/image/medium-low,format=auto,height=420/https://scriptrb.com/img/uploads/684692b90d5e1_106989035172456074720.webp" alt="" className="main__card-img" />
+          <div className="main__card-body">
+            <div className="main__card-title">Factory RNG</div>
+            <p className="main__card-description">Lorem ipsum dolor sit amet, consectetur adipisicing elit. Praesentium, exercitationem explicabo nisi aut laborum veritatis ad velit nihil sunt aliquam inventore a nulla mollitia quae, minus accusamus animi eos et.</p>
+            <div className="main__card-additional">
+              <button>View</button>
+              <div className="main__card-stat">
+                <p className="main__card-stat-downloads">131</p>
+                <p className="main__card-stat-date">09.06.2025</p>
+              </div>
+            </div>
+            </div>
+          </div>
+          <div className="main__card">
+          <img src="https://scriptrb.com/cdn-cgi/image/medium-low,format=auto,height=420/https://scriptrb.com/img/uploads/684692b90d5e1_106989035172456074720.webp" alt="" className="main__card-img" />
+          <div className="main__card-body">
+            <div className="main__card-title">Factory RNG</div>
+            <p className="main__card-description">Lorem ipsum dolor sit amet, consectetur adipisicing elit. Praesentium, exercitationem explicabo nisi aut laborum veritatis ad velit nihil sunt aliquam inventore a nulla mollitia quae, minus accusamus animi eos et.</p>
+            <div className="main__card-additional">
+              <button>View</button>
+              <div className="main__card-stat">
+                <p className="main__card-stat-downloads">131</p>
+                <p className="main__card-stat-date">09.06.2025</p>
+              </div>
+            </div>
+            </div>
+          </div>
+          <div className="main__card">
+          <img src="https://scriptrb.com/cdn-cgi/image/medium-low,format=auto,height=420/https://scriptrb.com/img/uploads/684692b90d5e1_106989035172456074720.webp" alt="" className="main__card-img" />
+          <div className="main__card-body">
+            <div className="main__card-title">Factory RNG</div>
+            <p className="main__card-description">Lorem ipsum dolor sit amet, consectetur adipisicing elit. Praesentium, exercitationem explicabo nisi aut laborum veritatis ad velit nihil sunt aliquam inventore a nulla mollitia quae, minus accusamus animi eos et.</p>
+            <div className="main__card-additional">
+              <button>View</button>
+              <div className="main__card-stat">
+                <p className="main__card-stat-downloads">131</p>
+                <p className="main__card-stat-date">09.06.2025</p>
+              </div>
+            </div>
+            </div>
+          </div>
+          <div className="main__card">
+          <img src="https://scriptrb.com/cdn-cgi/image/medium-low,format=auto,height=420/https://scriptrb.com/img/uploads/684692b90d5e1_106989035172456074720.webp" alt="" className="main__card-img" />
+          <div className="main__card-body">
+            <div className="main__card-title">Factory RNG</div>
+            <p className="main__card-description">Lorem ipsum dolor sit amet, consectetur adipisicing elit. Praesentium, exercitationem explicabo nisi aut laborum veritatis ad velit nihil sunt aliquam inventore a nulla mollitia quae, minus accusamus animi eos et.</p>
+            <div className="main__card-additional">
+              <button>View</button>
+              <div className="main__card-stat">
+                <p className="main__card-stat-downloads">131</p>
+                <p className="main__card-stat-date">09.06.2025</p>
+              </div>
+            </div>
+            </div>
+          </div>
+          <div className="main__card">
+          <img src="https://scriptrb.com/cdn-cgi/image/medium-low,format=auto,height=420/https://scriptrb.com/img/uploads/684692b90d5e1_106989035172456074720.webp" alt="" className="main__card-img" />
+          <div className="main__card-body">
+            <div className="main__card-title">Factory RNG</div>
+            <p className="main__card-description">Lorem ipsum dolor sit amet, consectetur adipisicing elit. Praesentium, exercitationem explicabo nisi aut laborum veritatis ad velit nihil sunt aliquam inventore a nulla mollitia quae, minus accusamus animi eos et.</p>
+            <div className="main__card-additional">
+              <button>View</button>
+              <div className="main__card-stat">
+                <p className="main__card-stat-downloads">131</p>
+                <p className="main__card-stat-date">09.06.2025</p>
+              </div>
+            </div>
+            </div>
+          </div>
+      </section>
+      <button className="main__load-button">Load More</button>
+   </main>
+  </>
+    
+
+  );
+};
+
+export default App;
---
+++ nastyhacks/src/components/Header.js
@@ -0,0 +1,32 @@
+function Header(){
+    return(
+        <header className='header container'>
+        <div className="header__left">
+          <div className="header__logo">
+            <img src="/svg/logo.svg" alt="" />
+          </div>
+          <div className="header__name">Script RB</div> 
+          <nav className="header__nav">
+            <ul className="header__nav-list">
+              <li className="header__nav-item">Home</li>
+              <li className="header__nav-item">Exploits</li>
+              <li className="header__nav-item">Tools</li>
+            </ul>
+          </nav>
+        </div>
+        <div className="header__right">
+          <div className="header__search">
+            <input type="text" className="header__search-input" />
+            <div className="header__search-icon">
+              <img src="/svg/search.svg" alt="" />
+            </div>
+          </div>
+          <div className="header__profile">
+            <img src="/svg/profile.svg" alt="" />
+          </div>
+        </div>
+     </header>
+    )
+}
+
+export default Header;
